a1f2fd08e8bbdf48751233704790c6d4a3e28f94
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 15:34:01 +0300

    Add detailed explanations and examples for strings in 7_String.py

--- 7_String.py
+++ 7_String.py
@@ -3,3 +3,23 @@
 # String in Python
 # A string is a sequence of characters.
 # A character is simply a symbol. For example, the English language has 26 characters.
+# Computers do not deal with characters, they deal with numbers (binary). 
+# Even though you may see characters on your screen, internally it is stored and manipulated as a combination of 0's and 1's.
+# This conversion of character to a number is called encoding, and the reverse process is decoding. ASCII and Unicode are some of the popular encoding used.
+# In Python, string is a sequence of Unicode character.
+# Unicode was introduced to include every character in all languages and bring uniformity in encoding.
+# You can use single or double quotes to create a string.
+# Multi-line strings can be denoted using triple quotes, ''' or """.
+# Like list and tuple, slicing operator [ ] can be used with string. Strings are immutable.
+
+# Example:
+
+# string
+
+str = "Bradley"
+print(str)
+
+# multiline string
+
+new_str = """This is a multiline string with more than one line code."""
+print(new_str)

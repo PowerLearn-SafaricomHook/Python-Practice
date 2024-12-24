417fb169803205647d432aa0d2de148ffe5d4253
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 15:16:11 +0300

    Add documentation and examples for numbers in Python

--- 6_Numbers.py
+++ 6_Numbers.py
@@ -1,2 +1,26 @@
 
 
+# Numbers in Python
+
+# Python supports integers, floating-point numbers and complex numbers. They are defined as int, float and complex class in Python.
+
+# Integers
+# Integers can be of any length, it is only limited by the memory available.
+# A floating-point number is accurate up to 15 decimal places. Integer and floating points are separated by decimal points. 1 is an integer, 1.0 is a floating-point number.
+# Complex numbers are written in the form, x + yj, where x is the real part and y is the imaginary part.
+# Imaginery part is written with a j suffix.
+
+# Example:
+
+# integer
+a = 10
+print(a, "is of type", type(a))
+
+# float
+a = 10.5
+print(a, "is of type", type(a))
+
+# complex number
+a = 1 + 2j
+print(a, "is comples number?", isinstance(1+2j, complex))
+print(a, "is of type", type(a))

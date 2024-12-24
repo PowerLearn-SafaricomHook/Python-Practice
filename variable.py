75e2c794fb8e7fd6ba61a29c7f484b269658e937
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 14:32:42 +0300

    Fix variable assignment and update comments in variable.py; add print statement for website

--- variable.py
+++ variable.py
@@ -1,8 +1,10 @@
 x=5
-print(x)
+# print(x)
 
-#Varabl examples assigning values to a variable and changing values of a variable
+#Variable examples assigning values to a variable and changing values of a variable
 
 website="Google.com"
 
 website="Yahoo.com"
+
+print(website)

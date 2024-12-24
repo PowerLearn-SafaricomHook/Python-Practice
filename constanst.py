61323eb0c48591a0969962fcdf8f0a20d926b460
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 14:38:49 +0300

    Add documentation for constants in constanst.py

--- constanst.py
+++ constanst.py
@@ -1,1 +1,6 @@
 
+# Constants in Python
+
+# Constants are usually defined on a module level and written in all capital letters with underscores separating words.
+# Constants are put into a separate module and imported.
+

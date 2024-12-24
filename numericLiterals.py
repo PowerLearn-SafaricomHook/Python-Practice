0772d7234daeed90d2ebbfe22578d626420a467a
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 14:42:23 +0300

    Add examples of numeric literals in numericLiterals.py

--- numericLiterals.py
+++ numericLiterals.py
@@ -1,1 +1,21 @@
 
+# Numeric Literals in python
+# Numeric Literals are immutable (unchangeable). Numeric literals can belong to 3 different numerical types: Integer, Float, and Complex.
+
+# Integer literals
+a = 0b1010 # Binary Literals
+b = 100 # Decimal Literal
+c = 0o310 # Octal Literal
+d = 0x12c # Hexadecimal Literal
+
+# Float literals
+float_1 = 10.5
+float_2 = 1.5e2
+
+# Complex literals
+x = 3.14j
+
+print(a, b, c, d)
+print(float_1, float_2)
+print(x, x.imag, x.real)
+

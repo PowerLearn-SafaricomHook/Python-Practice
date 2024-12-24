f09855baebf1033d921dfc6012ce4cfd3144268a
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 15:00:43 +0300

    Add example of an empty list to 5_CollectionsLiteral.py

--- 5_CollectionsLiteral.py
+++ 5_CollectionsLiteral.py
@@ -11,4 +11,6 @@
 # List literals are written within square brackets [ ]. Lists work similarly to strings -- use the len() function and square brackets [ ] to access data, with the first element at index 0.
 
 # List literals are written within square brackets [ ]. It is a mutable collection of items. You can add, remove, or delete elements from the list.
+# example:
 
+# empty list

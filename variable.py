07e1f8bdab458b0162cd3e8a60d8e607981ff435
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 14:37:02 +0300

    Refactor variable assignments and add multiple variable initialization in variable.py

--- variable.py
+++ variable.py
@@ -12,7 +12,18 @@ website="Yahoo.com"
 # assigning multiplle tomultiple variables
 
 num1,num2,num3=5,3.2,"Hello"
-print(num1)
-print(num2)
-print(num3)
+# print(num1)
+# print(num2)
+# print(num3)
+
+
+#Assigning the same value to multiple variables in one line
+
+x=y=z="same"
+
+print(x)
+print(y)
+print(z)
+
+
 

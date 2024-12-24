e9c8758e4bd695d52e8e65ac1532c53d1e0c6693
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 14:53:38 +0300

    Add Boolean literals examples and print statements to 4_Literals.py

--- 4_Literals.py
+++ 4_Literals.py
@@ -46,10 +46,26 @@ unicode= u"\u00dcnic\u00f6de"
 
 raw = r"raw \n string!"
 
-print(msg)
-print(char)
-print(multiline)
-print(unicode)
-print(raw)
+# print(msg)
+# print(char)
+# print(multiline)
+# print(unicode)
+# print(raw)
 
+# 
+
+# Boolean literals
+# A Boolean literal can have any of the two values: True or False.
+
+# In Python, True represents the value as 1 and False as 0.
+
+x = (1 == True)
+y = (1 == False)
+a = True + 4
+b = False + 10
+
+# print("x is", x)
+# print("y is", y)
+# print("a:", a)
+# print("b:", b)
 

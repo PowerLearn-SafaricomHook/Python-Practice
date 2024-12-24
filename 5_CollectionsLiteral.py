ce8661c7c9ca59258f642294c6ef13ca138aeb8b
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 15:17:34 +0300

    Add blank line for improved readability in 5_CollectionsLiteral.py

--- 5_CollectionsLiteral.py
+++ 5_CollectionsLiteral.py
@@ -8,6 +8,7 @@
 # Set literals
 
 # List literals
+
 # List literals are written within square brackets [ ]. Lists work similarly to strings -- use the len() function and square brackets [ ] to access data, with the first element at index 0.
 
 # List literals are written within square brackets [ ]. It is a mutable collection of items. You can add, remove, or delete elements from the list.
@@ -21,6 +22,7 @@ my_list = [1, 2, 3]
 
 # list with mixed data types
 my_list = [1, "Hello", 3.4]
+
 
 # nested list
 # nested list is a list within a list 

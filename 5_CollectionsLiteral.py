1855d297ede60e45259f6fae4d1c9706b5c426a5
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 15:00:04 +0300

    Add documentation for list literals in 5_CollectionsLiteral.py

--- 5_CollectionsLiteral.py
+++ 5_CollectionsLiteral.py
@@ -6,3 +6,8 @@
 # Tuple literals
 # Dict literals
 # Set literals
+
+# List literals
+# List literals are written within square brackets [ ]. Lists work similarly to strings -- use the len() function and square brackets [ ] to access data, with the first element at index 0.
+
+# List literals are written within square brackets [ ]. It is a mutable collection of items. You can add, remove, or delete elements from the list.

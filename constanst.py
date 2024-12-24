e37db6cf2959413d057de50e0ea6f5976b18a0c6
John-Mukhwana <johnbradill67@gmail.com>
2024-12-24 14:39:29 +0300

    Add constants for PI and GRAVITY in constanst.py and print their values

--- constanst.py
+++ constanst.py
@@ -4,3 +4,8 @@
 # Constants are usually defined on a module level and written in all capital letters with underscores separating words.
 # Constants are put into a separate module and imported.
 
+PI=3.14159
+GRAVITY=9.8
+
+print(PI)
+print(GRAVITY)

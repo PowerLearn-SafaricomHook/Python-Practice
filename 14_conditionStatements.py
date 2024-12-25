3f81e39d932acd27b87d212e28071059ab411e5a
John-Mukhwana <johnbradill67@gmail.com>
2024-12-25 13:27:06 +0300

    Fix formatting of comment for If Statement in 14_conditionStatements.py

--- 14_conditionStatements.py
+++ 14_conditionStatements.py
@@ -1,2 +1,4 @@
 
 # If Statement in python
+# If statement is used to check a condition: if the condition is true, we run a block of statements (called the if-block),
+#  else we process another block of statements (called the else-block).

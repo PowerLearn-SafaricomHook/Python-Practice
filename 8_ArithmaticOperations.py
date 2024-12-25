9c6f9439d12885548dd883c486614f282117772d
John-Mukhwana <johnbradill67@gmail.com>
2024-12-25 12:56:32 +0300

    Add examples for subtraction and multiplication operations in 8_ArithmaticOperations.py

--- 8_ArithmaticOperations.py
+++ 8_ArithmaticOperations.py
@@ -24,3 +24,27 @@ print(sum)  # 15
 # Subtraction
 # The subtraction operator - subtracts one number from another.
 
+# example:
+# subtract two numbers
+num1 = 5
+num2 = 10
+sub = num2 - num1
+print(sub)  # 5
+
+# Multiplication
+# The multiplication operator * multiplies two numbers.
+
+# example:
+# multiply two numbers
+num1 = 5
+num2 = 10
+mul = num1 * num2
+print(mul)
+
+# Division
+# The division operator / divides one number by another.
+
+# example:
+
+
+

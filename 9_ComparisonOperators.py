7827dd57d4b1b8b507d216492b1061cea07be9c5
John-Mukhwana <johnbradill67@gmail.com>
2024-12-25 12:59:52 +0300

    Add initial implementation of comparison operators in 9_ComparisonOperators.py

---
+++ 9_ComparisonOperators.py
@@ -0,0 +1,32 @@
+
+# Comparison Operators in Python
+
+# Equal to
+a = 5
+b = 5
+print(a == b)  # True
+
+# Not equal to
+a = 5
+b = 3
+print(a != b)  # True
+
+# Greater than
+a = 7
+b = 5
+print(a > b)  # True
+
+# Less than
+a = 3
+b = 5
+print(a < b)  # True
+
+# Greater than or equal to
+a = 5
+b = 5
+print(a >= b)  # True
+
+# Less than or equal to
+a = 3
+b = 5
+print(a <= b)  # True
